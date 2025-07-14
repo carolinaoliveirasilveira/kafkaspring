964168262840b486873bac77f76dc6dae97ece05
carolinaoliveirasilveira <carolinaoliveirasilveira@outlook.com>
2025-07-14 11:08:49 -0300

    Kafka update to support multiple brokers for high availability and scalability

--- src/main/java/com/kafkaspring/KafkaProducerController.java
+++ src/main/java/com/kafkaspring/KafkaProducerController.java
@@ -3,8 +3,8 @@ package com.kafkaspring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +13,11 @@ public class KafkaProducerController {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @GetMapping("/producer/{message}")
-    public ResponseEntity<String> producer(@PathVariable String message) {
+    @PostMapping("/producer")
+    public ResponseEntity<String> producer(@RequestBody String message) {
         kafkaTemplate.send("ecommerce.cliente2", message);
         return ResponseEntity.ok("Mensagem enviada ao tópico ecommerce.cliente2");
     }
+
 }
 
